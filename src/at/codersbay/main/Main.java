d7ae004249834f5f45ae90a509cc5c126cefaee0
Delta <christian.delta@gmx.at>
2019-08-05 09:56:16 +0200

    Edit Main Package

--- src/at/codersbay/main/Main.java
+++ src/at/codersbay/main/Main.java
@@ -1,9 +1,9 @@
-package at.codersbay;
+package at.codersbay.main;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World 2");
+        System.out.println("Hello World");
     }
 }
