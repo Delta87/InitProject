ad88f226a1f86ae40f0e4a53b4ac17368ad4ed05
Delta <christian.delta@gmx.at>
2019-08-06 09:14:14 +0200

    Aufgabe: Deklarieren und Initialisieren von Variablen

--- src/at/codersbay/main/Main.java
+++ src/at/codersbay/main/Main.java
@@ -6,5 +6,10 @@ public class Main {
 	// write your code here
         System.out.println("Hello World");
         System.out.println("Hello again");
+
+        boolean status = true;
+        int ganzZahl = 10;
+        String ZeichenKette = "Das ist eine Zeichenkette!";
+        double glietKommazahl = 123.123;
     }
 }
