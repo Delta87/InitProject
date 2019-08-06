8aba9715799cd96c64af2ffcc5650b7381b91757
Delta <christian.delta@gmx.at>
2019-08-06 09:33:14 +0200

    Aufgabe: Rechnen

--- src/at/codersbay/main/Main.java
+++ src/at/codersbay/main/Main.java
@@ -7,14 +7,33 @@ public class Main {
         //System.out.println("Hello World");
         //System.out.println("Hello again");
 
+
+
+        // Aufgabe 1
         boolean status = true;
         int ganzZahl = 10;
         String ZeichenKette = "Das ist eine Zeichenkette!";
         double gleitKommazahl = 123.123;
 
+        System.out.printf("Aufgabe 1: \n");
         System.out.println("Bool: " + status);
         System.out.println("Int: " + ganzZahl);
         System.out.println("String: " + ZeichenKette);
         System.out.println("Double: " + gleitKommazahl);
+
+
+        // Aufgabe 2
+        int Wert1 = 10;
+        int Wert2 = 2;
+
+        System.out.printf("\n");
+        System.out.printf("Aufgabe 2: \n");
+        Rechnen.addition(Wert1, Wert2);
+        Rechnen.substraction(Wert1, Wert2);
+        Rechnen.multiplication(Wert1, Wert2);
+        Rechnen.divisision(Wert1, Wert2);
+
+
+
     }
 }
---
+++ src/at/codersbay/main/Rechnen.java
@@ -0,0 +1,25 @@
+package at.codersbay.main;
+
+public class Rechnen {
+
+    public static void addition(int pZahl1, int pZahl2)
+    {
+        System.out.println(pZahl1 + " + " + pZahl2 + " = " + (pZahl1 + pZahl2));
+    }
+
+    public static void substraction(int pZahl1, int pZahl2)
+    {
+        System.out.println(pZahl1 + " - " + pZahl2 + " = " + (pZahl1 - pZahl2));
+    }
+
+    public static void multiplication(int pZahl1, int pZahl2)
+    {
+        System.out.println(pZahl1 + " * " + pZahl2 + " = " + (pZahl1 * pZahl2));
+    }
+
+    public static void divisision(int pZahl1, int pZahl2)
+    {
+        System.out.println(pZahl1 + " / " + pZahl2 + " = " + (pZahl1 / pZahl2));
+    }
+
+}
